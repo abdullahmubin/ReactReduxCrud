40c9e1327034f540adba59fa8b9b818ea1b53fc6
Abdullah al Mubin <amubin19@gmail.com>
2020-01-16 11:04:14 +0600

    Remove logo.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
